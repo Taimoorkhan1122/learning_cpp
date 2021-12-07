db2ad224050236561d3c8fabd312e29b40b300e1
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-12-07 06:02:31 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age  = 24;
+  string name  = "Taimoor khan";
+  const double PI = 3.1648324; 
+  float height = 5.8;
+  cout << "My Info:\nName: " << name << "\nAge: " << age << "\nheight: " << height;
+
+} 
