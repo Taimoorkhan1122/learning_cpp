9ac02f5ffe26ccdfdc2a13755644c0f58be8f65c
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-12-07 06:09:39 +0000

    basic datatypes and arithmetic operations

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 using namespace std;
 
 int main() {
+  // integer
   int age  = 24;
-  string name  = "Taimoor khan";
+  // double floating point
   const double PI = 3.1648324; 
+  // floating point upto 5 decimal
   float height = 5.8;
-  cout << "My Info:\nName: " << name << "\nAge: " << age << "\nheight: " << height;
+  // collection of characters
+  string name  = "Taimoor khan";
+
+  // console out
+  // cout << "My Info:\nName: " << name << "\nAge: " << age << "\nheight: " << height;
+  
+  // order of arithmatic operations * and / are prioritized 
+  cout << "3- 2 + 4 * 2 = " << 1 + 4 * 2 <<endl;
+  cout << "3- 2 + 4 * 2 = " << (1 + 4 )* 2 <<endl;
 
 } 
